3de8946329dfa2240ae54d49d0a2d7b62aa9311e
Harness Support <support@harness.io>
2024-01-16 21:13:29 +0000

    Project init

---
+++ src/test/java/com/kickoff/demo/KickoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kickoff.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
